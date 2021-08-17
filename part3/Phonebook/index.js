43f5b839e596eb98c55ba6ebfcddd7fc75f07b2f
Your Name <damian.pokrywka3@gmail.com>
2021-08-17 02:19:22 +0200

    Phonebook backend step8

---
+++ part3/Phonebook/index.js
@@ -0,0 +1,112 @@
+const { request, response } = require('express')
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+
+app.use(express.json())
+
+
+morgan.token('post', (request) => {
+  if (request.method === 'POST')
+    return(JSON.stringify(request.body))
+  else
+    return('')
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Wellcome to backend Phonebook!</h1>')
+})
+
+app.get('/api/persons/:id', (request, response)=>{
+    const id = Number(request.params.id)
+    const person = persons.find(persons=>persons.id===id)
+
+    if (person){
+        response.send(person)
+    }
+    else{
+        response.status(404).end()
+    }
+})
+
+app.get('/info', (request, response)=>{
+    const date = new Date()
+    response.send(`<p>Phonebook has info for ${persons.length} people</p>`+ date)
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.delete('/api/persons/:id', (request, response)=>{
+    const id = Number(request.params.id)
+    persons = persons.filter(person=>person.id!==id)
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response)=>{
+    const number_of_id = 10000
+    const generateID = Math.floor(Math.random()*number_of_id)
+    console.log(generateID)
+    const body = request.body
+
+    if(!body.name){
+      return response.status(400).json({
+        error: 'Name missing'
+      })
+    }
+    if(!body.number){
+      return response.status(400).json({
+        error: 'Number missing'
+      })
+    }
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateID,
+    }
+
+    if(persons.find(n=>n.name===person.name)){
+      return response.status(400).json({
+        error: 'Name must be unique'
+      })
+    }
+    else
+      {persons = persons.concat(person)
+      response.json(person)}
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
