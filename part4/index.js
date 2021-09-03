5071f4bea2a3edf94854b3fb5c64957ebca00fc5
Your Name <damian.pokrywka3@gmail.com>
2021-09-04 01:07:07 +0200

    4.2

---
+++ part4/index.js
@@ -0,0 +1,16 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
